aaf0042d2e2cc8a906559ba931d941f416221811
milind55555 <157528508+milind55555@users.noreply.github.com>
2025-06-04 06:02:15 +0000

    Data Types

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,23 @@
+const accountId = 8883; // const can't change
+let accountEmail = "abccer@gmail.com";
+var accountPassword = "mkmoreBoss0990";
+accountCity = "Akkalkot";
+let accountState
+
+
+// accountId = 5 // can't change
+
+accountEmail = "dfg.vom"
+accountPassword ="kkll"
+accountCity ="Pune"
+
+/*
+
+Dont't use "var"  because use in block scope and functional scope
+
+*/
+
+console.log(accountId)
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]) 
+
+
---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hello Brother") // we are using nodejs,not browser
+
+console.log(9+9)
+
+let name ="Milind"
+let age =21
+let IsLoggedIn = false
+let state = null
+//data type 
+//Primitive DT
+//number  2 to power 53
+//bigInteger
+//string => ""
+//boolean => true/false
+// null => standalone value
+// undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof null); //null is object type
+
+
