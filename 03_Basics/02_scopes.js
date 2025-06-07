947d8464f4ca65c4fc92268a15f29acc05aaa9ac
milind55555 <157528508+milind55555@users.noreply.github.com>
2025-06-07 06:34:52 +0000

    scope

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -16,6 +16,52 @@ console.log(a);
 // console.log(b);
 console.log(c);
 
+function one(){
+    const username="Atharva"
+    function two(){//closure child can access parent attribute username ca acsse
+        const website="youtube"
+        console.log(username);
+        
+    }
+    // console.log(website); //error occured due to scope 
+    two()
+    
+}
+one();
+
+if(true){
+    const username="Milind"
+    if(username === "Milind"){
+        const website = "youtube"
+        console.log(website+username);
+        
+    }
+    // console.log(website);//error due to scope 
+    
+}
+// console.log(username);//scopre is end if else
+
+//+++++++ interesting +++++//
+
+console.log(addOne(2));//no error 
+function addOne(num){
+        return num+1
+}
+
+// console.log(addTwo(4));//error got
+const addTwo =function(num){
+    return num+2
+}
+
+console.log(addTwo(4));
+
+
+
+
+
+
+
+
 
 
 
