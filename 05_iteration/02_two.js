41b92d5ee6e512744c7e41d36ec366c4b98d77c0
milind55555 <157528508+milind55555@users.noreply.github.com>
2025-06-07 08:31:31 +0000

    dowhileloop

---
+++ 05_iteration/02_two.js
@@ -0,0 +1,24 @@
+// while(condition){
+
+// }
+let index=0
+while(index <=10){
+    console.log(`Value of index is ${index}`);
+    index += 2
+
+}
+let myarr = ["flash","superman","batman"]
+let arr=0
+while(arr < myarr.length){
+    console.log(`arr is ${myarr[arr]}`);
+    arr += 1
+    
+}
+//do while
+let score = 11
+do{
+console.log(`Value of score is ${score}`);
+score++
+
+}while(score <= 10)
+
